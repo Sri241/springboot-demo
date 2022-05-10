58b3571d0246949b280d6f19e9fd210c0d356f40
Srilakshmi <chappagaddasrilakshmi@gmail.com>
2022-05-10 17:32:44 +0530

    Spring boot demo

---
+++ springboot-demo/src/main/java/com/wipro/velocity/controller/HelloWorldController.java
@@ -0,0 +1,31 @@
+package com.wipro.velocity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+//@RestController
+@Controller
+public class HelloWorldController {
+
+	//Executed with rest controller annotation - http://localhost:8080/hello
+	@GetMapping("/hello")
+    public String sayHello()
+    {
+        return "Hello World from Spring Boot";
+    }
+	
+	//Mapping for root - http://localhost:9090
+	@GetMapping("/")
+    public String index() {
+       return "index";  //returns view index.jsp
+    }
+   
+    @PostMapping("/test")
+    public String sayHello(@RequestParam("name") String name, Model model) {
+       model.addAttribute("name", name);
+       return "hello";  //returns view + model(data)
+    }
+}
